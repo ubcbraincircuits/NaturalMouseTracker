a90d3b9eb657ccb0c412c969ccbc785c3948249d
Judge24601 <braedenjury@gmail.com>
2019-05-21 10:01:15 -0700

    Updated RFID to work

--- RPi_Arduino_com_v7.py
+++ RPi_Arduino_com_v7.py
@@ -9,16 +9,15 @@ from picamera import PiCamera
 #camera = PiCamera()
 
 Trash_Data = [0,255,255,255,255,255,255,255,255,255,255,255]
-#ProT = [None]*18
-ProT = [None]*6
+ProT = [None]*18
 
+#Hex I2C Addresses of all ProTrinkets
 ProT [0] = 0x11
 ProT [1] = 0x12
 ProT [2] = 0x13
 ProT [3] = 0x14
 ProT [4] = 0x15
 ProT [5] = 0x16
-"""
 ProT [6] = 0x31
 ProT [7] = 0x32
 ProT [8] = 0x33
@@ -31,23 +30,28 @@ ProT [14] = 0x53
 ProT [15] = 0x54
 ProT [16] = 0x55
 ProT [17] = 0x56
-"""
-ProT_Dic = {ProT[0]:"1-1",ProT[1]:"1-2",ProT[2]:"1-3",ProT[3]:"1-4", ProT[4]:"1-5",ProT[5]:"1-6"}
-Mapping_Dic = { 0: [ProT[0], ProT[4],ProT[14]], 1: [ProT[4], ProT[13], ProT[17]],
-                2: [ProT[2], ProT[12], ProT[16]], 3: [ProT[1], ProT[5], ProT[15]],
-                4: [ProT[6], ProT[10]], 5: [ProT[7], ProT[11]], 6: [ProT[8]], 7:[ProT[9]]}
-"""
-,ProT[6]:"2-1",ProT[7]:"2-2",
-ProT[8]:"2-3",ProT[9]:"2-4",ProT[10]:"2-5",ProT[11]:"2-6",
-ProT[12]:"3-1",ProT[13]:"3-2",ProT[14]:"3-3",ProT[15]:"3-4",
-ProT[16]:"3-5",ProT[17]:"3-6"}
-"""
+ProT_Dic = {ProT[0]:"1-1",ProT[1]:"1-2",ProT[2]:"1-3",ProT[3]:"1-4",
+            ProT[4]:"1-5",ProT[5]:"1-6", ProT[6]:"2-1",ProT[7]:"2-2",
+            ProT[8]:"2-3",ProT[9]:"2-4",ProT[10]:"2-5",ProT[11]:"2-6",
+            ProT[12]:"3-1",ProT[13]:"3-2",ProT[14]:"3-3",ProT[15]:"3-4",
+            ProT[16]:"3-5",ProT[17]:"3-6"}
+#Adapted from ENPH 479 report by Yuan Tian, Ziyue Hu, and Becky Lin
+# Essentially, only readers at least 4 apart can be turned on simultaneously.
+# From testing, this unfortnuately leaves the middle units without a pair.
+Mapping_Dic = { 0: [0, 4], 1: [1, 5], 2: [6, 10], 3:[7, 11],
+                4: [12, 16], 5: [13, 17], 6: [14], 7: [15],
+                8: [8], 9:[9]}
+                #, 10: [2], 11:[3]}
+
 
 #ProT_arrange = [i for i in range(0,18)]
-ProT_arrange = [i for i in range(0,8)]
-#ProT_arrange = [i for i in range(0,6)]
+#ProT_arrange = [i for i in range(0,8)]
+ProT_arrange = [i for i in range(0,10)]
 #ProT_arrange = [0,8,6,2,1,7]
 
+#Write a number over the I2C bus.
+#Used to get a reading from the tag reader into the
+#ProTrinket buffer.
 def writeNumber (value,address_1):
     try:
         #with SMBus(1) as bus:
@@ -57,6 +61,8 @@ def writeNumber (value,address_1):
         print (e)
     return -1
 
+# Gets the last full tag in the ProTrinket serial buffer.
+# Converts this into a readable string.
 def readNumber(address_1):
     number1 = []
     try:
@@ -64,7 +70,6 @@ def readNumber(address_1):
         bus = SMBus(1)
         flag = False
         for i in range (0, 16):
-            time.sleep(0.01)
             if flag:
                 number1.append(chr(bus.read_byte(address_1)))
             else:
@@ -95,23 +100,23 @@ for j in range(10):
                 Data = Trash_Data
                 #time.sleep(0.01)
                 
-                for x in MappingDict[i]:
-                    writeNumber(val, x) 
-                time.sleep (0.16)
-                for x in MappingDict[i]:
-                    [Data,Time] = readNumber (x)
+                for x in Mapping_Dic[i]:
+                    writeNumber(var, ProT[x]) 
+                time.sleep (0.13)
+                for x in Mapping_Dic[i]:
+                    [Data,Time] = readNumber (ProT[x])
                     time.sleep  (0.01)
                     Time = time.time()
                     #Data = number1
-                    print (Data,Time)
                     if '\r' not in Data:
                         Data = []
                     if (Data != Trash_Data and Data != []):
                         try:
                             number =  ''.join(Data)
-                            number = number[0:number.index('\r') -3]
-                            f.write (str(int(number, 16))+" "+ProT_Dic[x]+" "+str(Time)+"\n")
-                            print (ProT_Dic[x])
+                            #The tag is always 10 characters long
+                            number = number[0:10]
+                            f.write (str(int(number, 16))+" "+ProT_Dic[ProT[x]]+" "+str(Time)+"\n")
+                            print (ProT_Dic[ProT[x]])
                             print (Data,Time)
                             print(number, int(number, 16))
                         except Exception as e:
