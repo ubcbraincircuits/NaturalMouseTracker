30ba602d50e4a7d5fa34bf329784e0bf44c74b76
Braeden <braedenjury@gmail.com>
2019-06-11 09:37:37 -0700

    Works, need to test with RFID

--- darknet_video.py
+++ darknet_video.py
@@ -10,6 +10,8 @@ import cv2
 import numpy as np
 import time
 import darknet
+import json
+from MouseTracker import MouseTracker
 
 def convertBack(x, y, w, h):
     xmin = int(round(x - (w / 2)))
@@ -44,6 +46,7 @@ altNames = None
 
 
 def YOLO(trialName, mice, RFID):
+    miceNum = len(mice)
 
     global metaMain, netMain, altNames
     configPath = "./yolov3-tiny-obj.cfg"
@@ -95,68 +98,95 @@ def YOLO(trialName, mice, RFID):
     # Create an image we reuse for each detect
     darknet_image = darknet.make_image(darknet.network_width(netMain),
                                     darknet.network_height(netMain),3)
-    frameCount = 0
+    frameCount = 1
+    lostTrackers = []
+    error = False
+    dummyTag = 0
     while True:
-        prev_time = time.time()
-        frameName = "tracking_system" + trialName + str(frameCount) + ".png"
-        frame_read = cv2.imread(frameName)
-        frameCount += 1
-        frame_rgb = cv2.cvtColor(frame_read, cv2.COLOR_BGR2RGB)
-        frame_resized = cv2.resize(frame_rgb,
-                                   (darknet.network_width(netMain),
-                                    darknet.network_height(netMain)),
-                                   interpolation=cv2.INTER_LINEAR)
-
+        try:
+            print("count:", frameCount)
+            prev_time = time.time()
+            frameName = "frameData/trackingsystem" + trialName + str(frameCount).zfill(5) + ".png"
+            frame_read = cv2.imread(frameName)
+            frameCount += 1
+            frame_rgb = cv2.cvtColor(frame_read, cv2.COLOR_BGR2RGB)
+            frame_resized = cv2.resize(frame_rgb,
+                                       (darknet.network_width(netMain),
+                                        darknet.network_height(netMain)),
+                                       interpolation=cv2.INTER_LINEAR)
+        except Exception as e:
+            #no more frames
+            break
         darknet.copy_image_from_bytes(darknet_image,frame_resized.tobytes())
 
         detections = darknet.detect_image(netMain, metaMain, darknet_image, thresh=0.25)
         image = cvDrawBoxes(detections, frame_resized)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         # print(1/(time.time()-prev_time))
-        updatedTrackers = []
+        updatedTags = []
         for detection in detections:
-
             x = detection[2][0]
             y = detection[2][1]
-            nearestTracker = sorted(list(filter(lambda x: x not in updatedTrackers, mice)), key= lambda x: x.distanceFromPos((x,y)))[0]
-            updatedTrackers.append(nearestTracker)
-            nearestTracker.updatePosition(position)
-        if len(detections) < len(mice):
+            nearestTracker = sorted(list(filter(lambda x: x.tag() not in updatedTags, mice)), key= lambda l: l.distanceFromPos((x,y)))[0]
+            updatedTags.append(nearestTracker.tag())
+            nearestTracker.updatePosition((x, y))
+        if len(detections) < len(mice) and error is False:
+            print("error :(")
+            print(detections)
+            print(updatedTags)
+            print(mice)
             error = True
-            for tracker in filter(lambda x: x not in updatedTrackers, mice):
-                lostTrackers.append(tracker)
-                mice.remove(tracker)
-                mice.append(MouseTracker(tracker.getPosition(), None))
-        if error == True:
-            anonymousTrackers = list(filter(lambda x: x.tag() is None, mice))
+            for tracker in filter(lambda x: x.tag() not in updatedTags, mice):
+                if tracker.tag() > 9999:
+                    #This is not a dummy tracker
+                    lostTrackers.append(tracker)
+                    mice.remove(tracker)
+                    mice.append(MouseTracker(tracker.getPosition(), dummyTag))
+                    dummyTag += 1
+        elif error == True:
+            print("err")
+            print(lostTrackers)
+            anonymousTrackers = list(filter(lambda x: x.tag() < 9999, mice))
             for line in RFID:
                 ln = line.split(';')
                 if ln[2] is frameName:
                     for tracker in lostTrackers:
                         if ln[0] is tracker.tag():
-                            nearestTracker = sorted(anonymousTrackers, key= lambda x: x.distanceFromPos((item for item in ln[1].strip('()\n').split(','))))[0]
-                            tracker.recordedPositions.extend(nearestTracker.recordedPositions)
+                            nearestAnon = sorted(anonymousTrackers, key= lambda x: x.distanceFromPos((item for item in ln[1].strip('()\n').split(','))))[0]
+                            tracker.recordedPositions.extend(nearestAnon.recordedPositions)
                             lostTrackers.remove(tracker)
-                            anonymousTrackers.remove(nearestTracker)
+                            mice.remove(nearestAnon)
+            if len(lostTrackers) == 1:
+                missingMouse = list(filter(lambda x: x.tag() < 9999, mice))[0]
+                lostTrackers[0].recordedPositions.extend(missingMouse.recordedPositions)
+                mice.append(lostTrackers[0])
+                lostTrackers = []
+                mice.remove(missingMouse)
+                error = False
             if len(lostTrackers) == 0:
                 error = False
         cv2.imshow('Demo', image)
         cv2.waitKey(3)
-    cap.release()
     out.release()
+    mouseDict = {}
+    for mouse in mouseTrackers:
+        mouseDict.update({mouse.tag(): mouse.recordedPositions})
+    with open("processed.json", "w") as outfile:
+        json.dump(mouseDict, outfile, ensure_ascii=False)
 
 if __name__ == "__main__":
     mouseTrackers = []
     #temp
-    mouseTrackers.append(MouseTracker((341, 266), 1))
-    mouseTrackers.append(MouseTracker((210, 166), 2))
-    mouseTrackers.append(MouseTracker((172, 82), 3))
-    fileName = "test.txt"
-    file = open(fileName, "r")
-    RFIDResponses = file.readlines()
-    for response in RFIDResponses:
-        ln = line.split(";")
-        if ln[2] is not "None":
-            mouseTrackers.append((item for item in ln[1].strip('()\n').split(',')), int(ln[0]))
-            break
+    mouseTrackers.append(MouseTracker((341, 266), 748204241))
+    mouseTrackers.append(MouseTracker((210, 166), 93112365))
+    mouseTrackers.append(MouseTracker((172, 82), 11118463))
+    RFIDResponses = []
+    # fileName = "test.txt"
+    # file = open(fileName, "r")
+    # RFIDResponses = file.readlines()
+    # for response in RFIDResponses:
+    #     ln = line.split(";")
+    #     if ln[2] is not "None":
+    #         mouseTrackers.append((item for item in ln[1].strip('()\n').split(',')), int(ln[0]))
+    #         break
     YOLO("trial", mouseTrackers, RFIDResponses)
